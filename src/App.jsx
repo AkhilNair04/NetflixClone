e2afb153f2bcecfd3900cb4894aff6694e49d48e
Akhil Nair <asn2k04@gmail.com>
2025-04-23 18:11:38 +0530

    Commit16

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,29 @@
 import React, { useEffect } from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
+import { Routes, Route, useNavigate, useLocation } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
 import Player from './pages/Player/Player';
 import { onAuthStateChanged } from 'firebase/auth';
 import { auth } from './firebase'; 
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 
 const App = () => {
   const navigate = useNavigate(); 
+  const location = useLocation();
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if (user) {
+      if (user && location.pathname === '/login') {
         console.log('Signed In');
         navigate('/');
-      } else {
+      } else if (!user && location.pathname !== '/login') {
         console.log('Signed Out');
         navigate('/login');
       }
     });
 
-    // Cleanup listener on unmount
     return () => unsubscribe();
-  }, [navigate]);
+  }, [navigate, location.pathname]);
 
   return (
     <div>
